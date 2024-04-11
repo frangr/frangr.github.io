6515227a269125e8d65c17b0ce667b4bc50bdea5
fr::gr <frrgrr9@gmail.com>
2024-04-11 15:09:10 +0200

    delete decl

--- FRVSE.js
+++ FRVSE.js
@@ -1,14 +1,14 @@
-function functest(p1)
-{
-	console.log("test: "+p1);
-}
-
 // *******FRVSE - Franto RISC-V Simple Emulator - by Grieco Francesco*******
 
 import { font8x8_basic } from './VIDEO_DATA.js'
 import { font8x8_block } from './VIDEO_DATA.js'
 import { font8x8_box } from './VIDEO_DATA.js'
 import { VGA_RGB_table } from './VIDEO_DATA.js'
+
+function functest(p1)
+{
+	console.log("test: "+p1);
+}
 
 //"MACRO"
 const MAX_INTERRUPT_NUMBER = 8;
