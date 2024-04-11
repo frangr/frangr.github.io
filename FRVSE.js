faef274efeaab76e34a904eb6a76edaff3eab45d
fr::gr <frrgrr9@gmail.com>
2024-04-11 14:57:25 +0200

    Update FRVSE.js

--- FRVSE.js
+++ FRVSE.js
@@ -3,10 +3,12 @@ function functest(p1)
 	console.log("test: "+p1);
 }
 
-//import { font8x8_basic } from 'VIDEO_DATA.js'
-//import { font8x8_block } from 'VIDEO_DATA.js'
-//import { font8x8_box } from 'VIDEO_DATA.js'
-//import { VGA_RGB_table } from 'VIDEO_DATA.js'
+// *******FRVSE - Franto RISC-V Simple Emulator - by Grieco Francesco*******
+
+import { font8x8_basic } from 'VIDEO_DATA.js'
+import { font8x8_block } from 'VIDEO_DATA.js'
+import { font8x8_box } from 'VIDEO_DATA.js'
+import { VGA_RGB_table } from 'VIDEO_DATA.js'
 
 //"MACRO"
 const MAX_INTERRUPT_NUMBER = 8;
