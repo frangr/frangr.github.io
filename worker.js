798e7dd93225fe4a072faad31de87ba7e973cc30
fr::gr <frrgrr9@gmail.com>
2024-04-22 00:11:56 +0200

    shared

--- worker.js
+++ worker.js
@@ -1,10 +1,19 @@
 // Listen for messages from the main page
+
+let sharedb = null;
+
 self.addEventListener('message', function(event) {
 	
 	console.log("EVENT: "+event)
 	
-	if(event.data === "mex")
+	if(event.data[0] === "mex")
 	{
+		sharedb = event.data[1];
+	}
+	if(event.data === "inc")
+	{
+		console.log("INC: "+sharedb[0])
+		sharedb[0]++;
 		self.postMessage("MEX");
 	}
 	if(event.data === "start")
