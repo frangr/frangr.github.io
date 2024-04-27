fa69b9b6bdc53f0a0d8b4f0e5a47ee49687c54d3
fr::gr <frrgrr9@gmail.com>
2024-04-27 14:33:51 +0200

    Update FRVSE.js

--- FRVSE.js
+++ FRVSE.js
@@ -778,7 +778,7 @@ function mem_device_controller(device, addr, data, rw, sz)
     }
     else if(rw == WRITE)
     {
-		console.log("MEM DEV4: "+data[0])
+		console.log("MEM DEV4: "+data8[0])
         switch(sz)
         {
         case ONE_BYTE:
