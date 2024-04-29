5a7238fb9462e6d3cf89308ef211805ed80218d8
fr::gr <frrgrr9@gmail.com>
2024-04-29 21:10:50 +0200

    Update FRVSE.js

--- FRVSE.js
+++ FRVSE.js
@@ -813,6 +813,7 @@ function mem_device_controller(device, addr, data, idx, rw, sz)
             break;
         case TWO_BYTE:
 			data[idx] = (device[addr+1] << 16) | device[addr];
+			console.log("DEVA: "+device[addr]+" .. "+device[addr+1]+" .. "+data[idx])
             break;
         case FOUR_BYTE:
 			data[idx] = (device[addr+3] << 24) | (device[addr+2] << 16) | (device[addr+1] << 8) | device[addr];
@@ -829,7 +830,6 @@ function mem_device_controller(device, addr, data, idx, rw, sz)
         case TWO_BYTE:
             device[addr] = data[idx];
             device[addr+1] = (data[idx] >> 8) & 0xFF;
-			console.log("DEVA: "+device[addr]+" .. "+device[addr+1])
             break;
         case FOUR_BYTE:
             device[addr] = data[idx];
