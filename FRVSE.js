409292df5c6fbd068d95ced64aae325238d71675
fr::gr <frrgrr9@gmail.com>
2024-04-29 12:59:35 +0200

    Update FRVSE.js

--- FRVSE.js
+++ FRVSE.js
@@ -909,8 +909,6 @@ function draw_character(addr, color)
 
     //addr *= 2;
 
-	(addr/40) (320*8)
-
 	console.log("addr1: "+addr);
     //addr += ((addr/8)/40)*(320*7); //0xA00;
 	addr += (addr/40)*(320*7); //0xA00;
