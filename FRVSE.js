72733cf919955d1cf53c0f8163a8eacc412f856a
fr::gr <frrgrr9@gmail.com>
2024-04-29 14:34:40 +0200

    Update FRVSE.js

--- FRVSE.js
+++ FRVSE.js
@@ -895,8 +895,6 @@ function draw_character(addr, color)
     if(addr > 999)
         return;
 
-	console.log("ADDR: "+addr)
-
     //return the bitmap of the character
     let character = get_char_array(CHARACTER);
 
@@ -1018,6 +1016,7 @@ function send_to_chipset(addr, data, idx, rw, sz)
 
     if(addr >= TEXT_MODE_MEMORY_START_ADDRESS && addr <= (TEXT_MODE_MEMORY_START_ADDRESS + TEXT_MODE_MEMORY_SIZE)-1)
     {
+		console.log("ADDR: "+addr-TEXT_MODE_MEMORY_START_ADDRESS)
         text_mode_controller(addr-TEXT_MODE_MEMORY_START_ADDRESS, data, idx, rw);
         return;
     }
