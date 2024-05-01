e1114fb2877fedd76f99584f371ec6a9ca689ae6
fr::gr <frrgrr9@gmail.com>
2024-05-02 00:36:23 +0200

    videomem

--- frvselib/frvselib.c
+++ frvselib/frvselib.c
@@ -2,11 +2,11 @@
 
 void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color)
 {
-	unsigned int addr = 0xF80C00+pos;
+	unsigned int addr = TEXT_MEMORY_START+pos;
 	*((unsigned int*)addr) = (font << 24) | (ascii << 16) | (char_color << 8) | background_color;
 }
 
 void stop_FRVSE()
 {
-	*((unsigned char*)0xF81BA0) = 0xFF;
+	*((unsigned char*)STOP_REGISTER_START) = 0xFF;
 }
--- frvselib/frvselib.h
+++ frvselib/frvselib.h
@@ -1,6 +1,9 @@
 #ifndef FRVSELIB_H
 #define FRVSELIB_H
 
+#define TEXT_MEMORY_START 0xF80C00
+#define STOP_REGISTER_START 0xF81BA0
+
 void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color);
 
 void stop_FRVSE();
