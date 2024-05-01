e7feec1cd65b724f02b19ba4651e2f539ff61b05
fr::gr <frrgrr9@gmail.com>
2024-05-01 22:05:02 +0200

    frvselib

--- frvselib/frvselib.c
+++ frvselib/frvselib.c
@@ -1,6 +1,12 @@
 #include "frvselib.h"
 
-void consolelog()
+void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color)
 {
-	*((unsigned char*)0xF81BA2) = 30;
+	unsigned int addr = 0xF80C00+pos;
+	*((unsigned int*)addr) = (font << 24) | (ascii << 16) | (char_color << 8) | background_color;
+}
+
+void stop_FRVSE()
+{
+	*((unsigned char*)0xF81BA0) = 0xFF;
 }
--- frvselib/frvselib.h
+++ frvselib/frvselib.h
@@ -1,1 +1,8 @@
-void consolelog();
+#ifndef FRVSELIB_H
+#define FRVSELIB_H
+
+void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color);
+
+void stop_FRVSE();
+
+#endif
