0c15fa2c63334bf43895a5f4bc9ae27c208e44e0
fr::gr <frrgrr9@gmail.com>
2024-05-02 01:11:59 +0200

    frvselib

--- frvselib/frvselib.c
+++ frvselib/frvselib.c
@@ -1,12 +1,101 @@
 #include "frvselib.h"
 
+void stop_FRVSE()
+{
+	*((unsigned char*)STOP_REGISTER_ADDRESS) = 0xFF;
+}
+
+void write_string(short pos, char* str, short strlen, unsigned char font, unsigned char char_color, unsigned char background_color)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	for(short i = 0; i < strlen; i++)
+		*((unsigned int*)addr++) = (font << 24) | (str[i] << 16) | (char_color << 8) | background_color;
+}
+
+//TEXT MEMORY FUNCTIONS
 void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color)
 {
-	unsigned int addr = TEXT_MEMORY_START+pos;
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
 	*((unsigned int*)addr) = (font << 24) | (ascii << 16) | (char_color << 8) | background_color;
 }
 
-void stop_FRVSE()
+void redraw_text_memory()
 {
-	*((unsigned char*)STOP_REGISTER_START) = 0xFF;
+	unsigned int* addr = (unsigned int*)TEXT_MODE_MEMORY_START_ADDRESS;
+	for(short i = 0; i < 1000; i++)
+	{
+		*addr = *addr;
+		addr++;
+	}	
+}
+
+void character_set_font(unsigned short pos, unsigned char font)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	textword &= 0x00FFFFFF;
+	textword |= font << 24;
+	*((unsigned int*)addr) = textword;
+}
+
+void character_set_ascii(unsigned short pos, unsigned char ascii)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	textword &= 0xFF00FFFF;
+	textword |= ascii << 16;
+	*((unsigned int*)addr) = textword;
+}
+
+void character_set_char_color(unsigned short pos, unsigned char char_color)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	textword &= 0xFFFF00FF;
+	textword |= char_color << 8;
+	*((unsigned int*)addr) = textword;
+}
+
+void character_set_background_color(unsigned short pos, unsigned char bck_color)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	textword &= 0xFFFFFF00;
+	textword |= bck_color;
+	*((unsigned int*)addr) = textword;
+}
+
+unsigned int get_character(short pos)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	return textword;
+}
+
+char character_get_font(short pos)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	return (textword >> 24) & 0xFF;
+}
+
+char character_get_ascii(short pos)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	return (textword >> 16) & 0xFF;
+}
+
+char character_get_char_color(short pos)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	return (textword >> 8) & 0xFF;
+}
+
+char character_get_bck_color(short pos)
+{
+	unsigned int addr = TEXT_MODE_MEMORY_START_ADDRESS+pos;
+	unsigned int textword = *((unsigned int*)addr);
+	return textword & 0xFF;
 }
--- frvselib/frvselib.h
+++ frvselib/frvselib.h
@@ -1,10 +1,52 @@
 #ifndef FRVSELIB_H
 #define FRVSELIB_H
 
-#define TEXT_MEMORY_START 0xF80C00
-#define STOP_REGISTER_START 0xF81BA0
+#define ROM_MEMORY_START_ADDRESS 0x0
+#define ROM_MEMORY_SIZE 0x2DC6C0 //3MB
+
+#define RAM_START_ADDRESS (ROM_MEMORY_START_ADDRESS + ROM_MEMORY_SIZE) //0x2DC6C0
+#define RAM_SIZE 0x4C4B40 //5MB
+
+#define MM_START_ADDRESS (RAM_START_ADDRESS + RAM_SIZE) //0x7A1200
+#define MM_SIZE 0x7A1200 //8MB
+
+#define VIDEO_MEMORY_START_ADDRESS (MM_START_ADDRESS + MM_SIZE) //0xF42400
+#define VIDEO_MEMORY_SIZE 0x3E800
+
+#define TEXT_MODE_MEMORY_START_ADDRESS (VIDEO_MEMORY_START_ADDRESS + VIDEO_MEMORY_SIZE) //0xF80C00
+#define TEXT_MODE_MEMORY_SIZE 0xFA0 //4000 bytes (1000 4-byte word for each character)
+
+#define STOP_REGISTER_ADDRESS (TEXT_MODE_MEMORY_START_ADDRESS + TEXT_MODE_MEMORY_SIZE) //F81BA0
+
+#define KEYCODE_REGISTER_ADDRESS STOP_REGISTER_ADDRESS+1 //F81BA1
+
+#define DEBUG_VALUE_PORT_ADDRESS KEYCODE_REGISTER_ADDRESS+1 //F81BA2
 
 void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color);
+
+void redraw_text_memory()
+
+void write_string(short pos, char* str, short strlen, unsigned char font, unsigned char char_color, unsigned char background_color);
+
+void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color);
+
+void character_set_font(unsigned short pos, unsigned char font);
+
+void character_set_ascii(unsigned short pos, unsigned char ascii);
+
+void character_set_char_color(unsigned short pos, unsigned char char_color);
+
+void character_set_background_color(unsigned short pos, unsigned char bck_color);
+
+unsigned int get_character(short pos);
+
+char character_get_font(short pos);
+
+char character_get_ascii(short pos);
+
+char character_get_char_color(short pos);
+
+char character_get_bck_color(short pos);
 
 void stop_FRVSE();
 
