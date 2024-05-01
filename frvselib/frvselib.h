877ec17392f69f36a06045bf6d760db3592acb7a
fr::gr <frrgrr9@gmail.com>
2024-05-02 01:12:10 +0200

    Update frvselib.h

--- frvselib/frvselib.h
+++ frvselib/frvselib.h
@@ -24,7 +24,7 @@
 
 void write_character(unsigned short pos, unsigned char font, unsigned char ascii, unsigned char char_color, unsigned char background_color);
 
-void redraw_text_memory()
+void redraw_text_memory();
 
 void write_string(short pos, char* str, short strlen, unsigned char font, unsigned char char_color, unsigned char background_color);
 
